f03ee8f22047253eed19e92a0b1526892364f6e0
GeraldRyan <gerald.ryan40@protonmail.com>
2021-02-23 11:59:19 -0600

    Added fahrenheit to celsius converter

--- ClassCode/src/classcode/LoopExamples.java
+++ ClassCode/src/classcode/LoopExamples.java
@@ -7,13 +7,18 @@ public class LoopExamples {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int inputSec = sc.nextInt();
+		System.out.println("Enter a temperature in fahrenheit");
+		double f = sc.nextDouble();
+		double c = (5.0/9) * (f -32);
+		System.out.println(f + " fahrenheit is " + c + "celsius");
+		sc.close();
+		// int inputSec = sc.nextInt();
 		
-		int min, sec; 
+		// int min, sec; 
 		
-		min = inputSec / 60;
-		sec = inputSec % 60;
-		System.out.println(min + "min and " + sec + " sec");
+		// min = inputSec / 60;
+		// sec = inputSec % 60;
+		// System.out.println(min + "min and " + sec + " sec");
 	}
 	
 }
--- ClassCode/src/classcode/P.java
+++ ClassCode/src/classcode/P.java
@@ -1,26 +1,26 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-public class MainForGraphics1
-{
-public static void main(String[] args)
-{
+// import javax.swing.JFrame;
+// import javax.swing.JPanel;
+// public class MainForGraphics1
+// {
+// public static void main(String[] args)
+// {
 
-JFrame frame = new JFrame();
-
-
-frame.setSize(1920,1080);
+// JFrame frame = new JFrame();
 
 
-frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+// frame.setSize(1920,1080);
 
 
-Panel00 panel = new Panel00();
+// frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+// Panel00 panel = new Panel00();
    
 
-frame.add(panel);
+// frame.add(panel);
 
-frame.setVisible(true);
+// frame.setVisible(true);
 
 
-}
-}
+// }
+// }
